e81b15f6d42a3e8ef1226a652d1aad95e9ef704e
Your Name <you@example.com>
2024-03-17 19:18:17 -0700

    lockbox

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+def canUnlockAll(boxes):
+    if not boxes:
+        return False
+
+    visited = set()
+    visited.add(0)
+
+    queue = [0]
+
+    while queue:
+        current_box = queue.pop(0)
+        keys = boxes[current_box]
+
+        for key in keys:
+            if key < len(boxes) and key not in visited:
+                visited.add(key)
+                queue.append(key)
+
+    return len(visited) == len(boxes)
+
